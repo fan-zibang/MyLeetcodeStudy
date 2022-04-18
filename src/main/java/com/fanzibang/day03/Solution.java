2dfde7bcddc9779baa9dac7aa7deea873d3d777d
fan-zibang <fzb380996870@163.com>
2022-04-18 17:15:06 +0800

    leetcode hot100 03

--- src/main/java/com/fanzibang/day03/Solution.java
+++ src/main/java/com/fanzibang/day03/Solution.java
@@ -1,5 +1,7 @@
 package com.fanzibang.day03;
 
+import java.util.HashMap;
+
 public class Solution {
     /**
      * 给定一个字符串 s ，请你找出其中不含有重复字符的最长子串的长度。
@@ -25,8 +27,27 @@ public class Solution {
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
 
+    /**
+     * 1.循环遍历字符串
+     * 2.判断map里是否存在字符
+     *   2.1 如果存在字符，更新left的位置
+     *   2.2 如果不存在，将当前字符放入map中，并更新max
+     * 3.返回max
+     *
+     */
     public int lengthOfLongestSubstring(String s) {
+        if (s.equals("")) return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int max = 0;
 
-        return 0;
+        for (int i = 0;i < s.length();i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i-left+1);
+        }
+        return max;
     }
 }
